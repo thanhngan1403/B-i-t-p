f9c5c20562599ecc99deb56f7e44118c23f276dc
thanhngan1403 <ngan_dth225701@student.agu.edu.vn>
2024-09-22 22:26:30 +0700

    Cập nhật

---
+++ DTH225701.Chuong02.Bai10.py
@@ -0,0 +1,9 @@
+'''
+(a) x = x + 1       -> x += 1
+(b) x = x / 2 		   x /= 2
+(c) x = x - 1 		   x -= 1
+(d) x = x + y 		   x += y
+(e) x = x - (y + 7)	   x -= (y+7)
+(f) x = 2*x			   x *= 2
+(g) number_of_closed_cases = number_of_closed_cases + 2*ncc  -> number_of_closed_cases += 2*ncc
+'''
