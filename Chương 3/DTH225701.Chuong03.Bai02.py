5bde5d46f1da87b4d4bdf49edecda700a35804f3
thanhngan1403 <ngan_dth225701@student.agu.edu.vn>
2024-09-23 09:53:20 +0700

    n

---
+++ Chương 3/DTH225701.Chuong03.Bai02.py
@@ -0,0 +1,12 @@
+print("Chương trình đếm số ngày trong 1 tháng")
+thang=int(input("Nhập vào một tháng: "))
+if thang in (1,3,5,7,10,12):
+    print("Tháng",thang,"có 31 ngày")
+elif thang in(4,6,8,9,11):
+    print("Tháng",thang,"có 30 ngày")
+elif thang==2:
+    print("Nhập năm: ")
+    year=
+    if()
+
+
