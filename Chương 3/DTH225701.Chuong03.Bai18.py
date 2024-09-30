2594e9b21812b9e1fc8beb735cdd2f9c63573608
thanhngan1403 <ngan_dth225701@student.agu.edu.vn>
2024-09-30 19:58:06 +0700

    2

--- Chương 3/DTH225701.Chuong03.Bai18.py
+++ Chương 3/DTH225701.Chuong03.Bai18.py
@@ -1,13 +1,35 @@
-def draw_square(n):
-    for i in range(n):
-        for j in range(n):
-            if i == 0 or i == n-1 or j == 0 or j == n-1:
-                print('*', end=' ')
+n = int(input("Nhap n:"))
+print("Hinh 1")
+for i in range(n):
+    for j in range(n):
+        if (i==0 or i==n-1 or j==0 or j==n-1):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+print("\nHinh 2")
+for i in range(n):
+    for j in range(n):
+        if j >= (n-1)-i:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+print("\nHinh3")
+center = n//2
+for i in range(n):
+    for j in range(n):
+        if i <= center:
+            if(j==0 or i==j or i==center):
+                print("*", end=" ")
             else:
-                print(' ', end=' ')
-        print()
-
-n = int(input("Nhập chiều cao n: "))
-draw_square(n)
-
+                print(" ", end=" ")
+        else:
+            if(i==j or j==(n-1)):
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+    print()
 
--- Chương 3/DTH225702.Chuong03.Bai19.py
+++ Chương 3/DTH225702.Chuong03.Bai19.py
@@ -1,1 +1,10 @@
-
+x = int(input("Nhập x: "))
+n = int(input("Nhập n: "))
+s = x  # Bắt đầu với x là phần tử đầu tiên trong chuỗi
+for i in range(1, n+1):
+    tu = x**(2*i+1)  # Tính x mũ (2i+1), chỉ lấy các số lẻ
+    mau = 1
+    for j in range(1, (2*i+2)):  # Tính giai thừa của (2i+1)
+        mau *= j
+    s += tu / mau
+print("S({0},{1})={2}".format(x, n, s))
